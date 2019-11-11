ce287b4cc9d3943c3e52df1ba45d258a28505698
dylanlangerman1 <dlangerm@asu.edu>
2019-11-10 18:56:47 -0700

    add help change form a bit

--- accountmanager/ServerSideUSAA.asmx.cs
+++ accountmanager/ServerSideUSAA.asmx.cs
@@ -21,36 +21,32 @@ namespace accountmanager
         public List<String> HelperAPI(string url = "",string text = "")
         {
             List<String> Responses = new List<string>();
-            string APIresponse = APICall(3,url,text);
-            JObject json = JObject.Parse(APIresponse);
-            string twitter = ParseTwitter(json.GetValue("summary").ToString());
+            
+            string twitter = ParseTwitter(APICall(3, url, text));
             Responses.Add(twitter);
             System.Threading.Thread.Sleep(3000);
-            APIresponse = APICall(5,url, text);
-            json = JObject.Parse(APIresponse);
-            string chatbot = ParseChatbot(json.GetValue("summary").ToString());
+
+            string chatbot = ParseChatbot(APICall(4, url, text));
             Responses.Add(chatbot);
 
             //Sleep(3000) makes application wait 3 second to avoid a time out for API calls
             System.Threading.Thread.Sleep(3000);
-            APIresponse = APICall(4, url, text);
-            json = JObject.Parse(APIresponse);
-            string facebook = ParseFacebook(json.GetValue("summary").ToString());
+            
+            string facebook = ParseFacebook(APICall(5, url, text));
             Responses.Add(facebook);
-            APIresponse = APICall(3, url, text);
-            json = JObject.Parse(APIresponse);
-            string abbreviated = ParseAbbreviated(json.GetValue("summary").ToString());
+            
+            string abbreviated = ParseAbbreviated(APICall(6, url, text));
             Responses.Add(abbreviated);
             return Responses;
 
         }
         [WebMethod]
-        public string hash()
+        public string hash(string text)
         {
-            var client = new RestClient("https://api.ritekit.com/v1/stats/multiple-hashtags");
+            var client = new RestClient("https://private-ceeab-ritekit.apiary-proxy.com/v1/stats/auto-hashtag?client_id=60f74b84a32620ad29eb0bcef4580617bc91a1560fb2&post="+text+"&maxHashtags=3&hashtagPosition=end");
             var request = new RestRequest(Method.POST);
             request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", "client_id=0559d8e4ffcecd87c6e9098e2cae536b896471dfbd92&post=Is%20artificial%20intelligence%20the%20future%20of%20customer%20service%3F&maxHashtags=2&hashtagPosition=auto", ParameterType.RequestBody);
+            request.AddParameter("application/x-www-form-urlencoded", "post=Is%20artificial%20intelligence%20the%20future%20of%20customer%20service%", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             return response.Content;
 
@@ -63,6 +59,10 @@ namespace accountmanager
 
         public string ParseTwitter(string text)
         {
+            text = hash(text);
+            JObject json = JObject.Parse(text);
+            text = json.GetValue("post").ToString();
+            
             string[] greetings = { "Hello, Twitter follower.","Hi,","Real talk,","Greetings,"
             ,"What's good!"};
             Random rnd = new Random();
@@ -93,11 +93,9 @@ namespace accountmanager
             request.AddHeader("content-type", "application/x-www-form-urlencoded");
             request.AddParameter("application/x-www-form-urlencoded", "key=980a54fdf7f02c37902b078872ce6eb0&sentences=" + sentenceNum.ToString()+"&txt="+text+"&url="+url+"&doc=", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
-            return response.Content;
-            
+            JObject json = JObject.Parse(response.Content);
+            return json.GetValue("summary").ToString();
+
         }
-
-
-
 	}
 }
