f2e68be7652e4a4f989f4b73e0f4a001207a4276
dylanlangerman1 <dlangerm@asu.edu>
2019-11-11 00:09:58 -0700

    output table

--- accountmanager/ServerSideUSAA.asmx.cs
+++ accountmanager/ServerSideUSAA.asmx.cs
@@ -23,14 +23,15 @@ namespace accountmanager
             List<String> Responses = new List<string>();
             
             string twitter = ParseTwitter(APICall(3, url, text));
+            
             Responses.Add(twitter);
-            System.Threading.Thread.Sleep(3000);
+            //System.Threading.Thread.Sleep(3000);
 
             string chatbot = ParseChatbot(APICall(4, url, text));
             Responses.Add(chatbot);
 
             //Sleep(3000) makes application wait 3 second to avoid a time out for API calls
-            System.Threading.Thread.Sleep(3000);
+            //System.Threading.Thread.Sleep(3000);
             
             string facebook = ParseFacebook(APICall(5, url, text));
             Responses.Add(facebook);
@@ -43,33 +44,51 @@ namespace accountmanager
         [WebMethod]
         public string hash(string text)
         {
-            var client = new RestClient("https://private-ceeab-ritekit.apiary-proxy.com/v1/stats/auto-hashtag?client_id=60f74b84a32620ad29eb0bcef4580617bc91a1560fb2&post="+text+"&maxHashtags=3&hashtagPosition=end");
+            var client = new RestClient("https://private-ceeab-ritekit.apiary-proxy.com/v1/stats/auto-hashtag?client_id=60f74b84a32620ad29eb0bcef4580617bc91a1560fb2&post="+text+"&maxHashtags=2&hashtagPosition=end");
             var request = new RestRequest(Method.POST);
             request.AddHeader("content-type", "application/x-www-form-urlencoded");
             request.AddParameter("application/x-www-form-urlencoded", "post=Is%20artificial%20intelligence%20the%20future%20of%20customer%20service%", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
-            return response.Content;
+           
+            JObject json = JObject.Parse(response.Content);
+            return json.GetValue("post").ToString();
 
         }
         public string QuickParse(string text)
         {
             return text.Replace("[...]", "");
         }
-       
 
-        public string ParseTwitter(string text)
+        int s = 3;
+        public string ParseTwitter(string otext)
         {
-            text = hash(text);
-            JObject json = JObject.Parse(text);
-            text = json.GetValue("post").ToString();
+            string text = otext;
             
+            //text = hash(text);
+            //JObject json = JObject.Parse(text);
+            //text = json.GetValue("post").ToString();
+
             string[] greetings = { "Hello, Twitter follower.","Hi,","Real talk,","Greetings,"
             ,"What's good!"};
             Random rnd = new Random();
             string greeting = greetings[rnd.Next(0, greetings.Count())];
             text = QuickParse(text);
             text = greeting + text;
-            return text;
+            if (text.Length > 280)
+            {
+                s--;
+                text = ParseTwitter(APICall(s,"", otext));
+                
+            }
+            string hastpost = hash(text);
+            if (hastpost.Length < 281)
+            {
+                return hastpost;
+            }
+            else
+                return text;
+
+            
         }
         public string ParseChatbot(string text)
         {
@@ -88,13 +107,25 @@ namespace accountmanager
         }
         public string APICall(int sentenceNum, string url = "",string text = "")
         {
-            var client = new RestClient("https://api.meaningcloud.com/summarization-1.0");
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", "key=980a54fdf7f02c37902b078872ce6eb0&sentences=" + sentenceNum.ToString()+"&txt="+text+"&url="+url+"&doc=", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
-            JObject json = JObject.Parse(response.Content);
-            return json.GetValue("summary").ToString();
+            string ntext;
+            try
+            {
+                var client = new RestClient("https://api.meaningcloud.com/summarization-1.0");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/x-www-form-urlencoded");
+                request.AddParameter("application/x-www-form-urlencoded", "key=980a54fdf7f02c37902b078872ce6eb0&sentences=" + sentenceNum.ToString() + "&txt=" + text + "&url=" + url + "&doc=", ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                JObject json = JObject.Parse(response.Content);
+                return json.GetValue("summary").ToString();
+            }
+            catch (Exception)
+            {
+
+                System.Threading.Thread.Sleep(3000);
+                ntext = APICall(sentenceNum, url, text);
+            }
+            return ntext;
+
 
         }
 	}
