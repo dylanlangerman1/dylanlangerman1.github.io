a15c391b1a85b16864b211946a6143b70ac60fc8
dylanlangerman1 <dlangerm@asu.edu>
2019-11-10 19:17:24 -0700

    add help change form a bit

---
+++ accountmanager/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace accountmanager
+{
+    public class Image
+    {
+        public int picID;
+        public string keyWord;
+        public string image;
+        public int likes = 0;
+
+    }
+}
